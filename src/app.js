7ac74a7e39bf13d05825b8794f90b2e728089cbd
Ahmad Khan <mkhan2@andrew.cmu.edu>
2020-12-17 14:14:18 +0300

    Make page dynamic

--- src/app.js
+++ src/app.js
@@ -4,7 +4,15 @@ const express = require('express')
 const app = express()
 publicPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicPath))
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Ahmad Khan'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
