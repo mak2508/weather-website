cf748e459131eb646e1377542f0b41bf297b3fd8
Ahmad Khan <mkhan2@andrew.cmu.edu>
2020-12-18 22:13:53 +0300

    Copy in weather app utility files

---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, location, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=f85f1c65494f7ddad90b416d7e12f35e&query=${latitude},${longitude}&units=m`
+
+  request({url, json: true}, (error, { body } = {}) => {
+    if (error) {
+      callback("Unable to connect to the network")
+    } else if (body.error) {
+      callback("Unable to fetch weather for given query")
+    } else {
+      callback(undefined, body.current.weather_descriptions[0] +
+                 ' weather today in ' +
+                 location +
+                 '. It is ' +
+                 body.current.temperature +
+                 ' degrees out, but it feels like ' +
+                 body.current.feelslike +
+                 ' degrees')
+    }
+  })
+}
+
+module.exports = forecast
