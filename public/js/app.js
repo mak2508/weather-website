e4ccf9bfd0749e93e7bf30fb47e65902c50957c6
Ahmad Khan <mkhan2@andrew.cmu.edu>
2020-12-19 10:36:32 +0300

    Add browser interface for weather API

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,18 @@
-console.log("Client side JS file")
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch('http://localhost:3000/weather?address=doha').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data.location)
-      console.log(data.forecast)
-    }
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data.location)
+        console.log(data.forecast)
+      }
+    })
   })
 })
