cb198eea918c8439e3c4da466e6637b7e4aea20a
Ahmad Khan <mkhan2@andrew.cmu.edu>
2020-12-19 10:22:07 +0300

    Add browser-side JS functionality to fetch weather

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 console.log("Client side JS file")
+
+fetch('http://localhost:3000/weather?address=doha').then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error)
+    } else {
+      console.log(data.location)
+      console.log(data.forecast)
+    }
+  })
+})
